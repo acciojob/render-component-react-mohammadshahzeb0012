6057fad2a7adf7fc566d8446be37dbf1c08374e0
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-04-16 07:18:47 +0000

    2nd

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1>a</h1>
-      <p> am learning React. My life is getting better.</p>
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
